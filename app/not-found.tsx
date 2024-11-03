d0fdc0be5ab6b32f5bbd1a843daa4e9cd44a87da
Maor Rocky <maorrocky@gmail.com>
2024-11-03 19:14:10 +0200

    clerk added

---
+++ app/not-found.tsx
@@ -0,0 +1,32 @@
+﻿import React from "react";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
+
+function NotFoundPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen p-4">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold text-primary mb-4">404</h1>
+        <h2 className="text-2xl font-semibold mb-4">Page Not Found</h2>
+        <p className="text-muted-foreground mb-8 max-w-md">
+          Dont worry, it happens to the best of us.
+        </p>
+        <div className="flex flex-col sm:flex-row justify-center gap-4">
+          <Link
+            href="/"
+            className="flex items-center justify-center px-4 py-2 bg-primary text-white rounded-md hover:bg:primary/80
+            transition-colors"
+          >
+            <ArrowLeft className="h-4 w-4 mr-2" />
+            Go Back To Dashboard
+          </Link>
+        </div>
+        <footer className="text-sm mt-12 text-muted-foreground">
+          If you believe this is a mistake, please contact us
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default NotFoundPage;
--- components/BreadcrumbHeader.tsx
+++ components/BreadcrumbHeader.tsx
@@ -7,12 +7,14 @@ import {
   BreadcrumbLink,
   BreadcrumbList,
 } from "@/components/ui/breadcrumb";
+import { MobileSidebar } from "@/components/Sidebar";
 
 function BreadcrumbHeader() {
   const pathName = usePathname();
   const paths = pathName === "/" ? [""] : pathName?.split("/");
   return (
     <div className="flex items-center flex-start">
+      <MobileSidebar />
       <Breadcrumb>
         <BreadcrumbList>
           {paths.map((path, index) => (
