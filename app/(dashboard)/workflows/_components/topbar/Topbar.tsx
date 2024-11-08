0f2d79210934740bd8c9582b712785e932bae76b
Maor Rocky <maorrocky@gmail.com>
2024-11-08 10:38:48 +0200

    started working on top bar

---
+++ app/(dashboard)/workflows/_components/topbar/Topbar.tsx
@@ -0,0 +1,22 @@
+﻿"use client";
+
+import TooltipWrapper from "@/components/TooltipWrapper";
+import { Button } from "@/components/ui/button";
+import { router } from "next/client";
+import { ChevronLeftIcon } from "lucide-react";
+
+function Topbar() {
+  return (
+    <header className="flex p-2 border-b-pink-200 border-separate justify-between w-full h-[60px] sticky top-0 bg-background z-10">
+      <div className="flex gap-1 flex-1">
+        <TooltipWrapper content="Back">
+          <Button variant="ghost" size="icon" onClick={() => router.back()}>
+            <ChevronLeftIcon size="20" />
+          </Button>
+        </TooltipWrapper>
+      </div>
+    </header>
+  );
+}
+
+export default Topbar;
--- app/workflow/editor/_components/Editor.tsx
+++ app/workflow/editor/_components/Editor.tsx
@@ -2,11 +2,13 @@
 import { Workflow } from "@prisma/client";
 import { ReactFlowProvider } from "@xyflow/react";
 import FlowEditor from "@/app/workflow/editor/_components/FlowEditor";
+import Topbar from "@/app/(dashboard)/workflows/_components/topbar/Topbar";
 
 function Editor({ workflow }: { workflow: Workflow }) {
   return (
     <ReactFlowProvider>
       <div className="flex flex-col h-full w-full overflow-hidden">
+        <Topbar />
         <section className="flex h-full overflow-auto">
           <FlowEditor workflow={workflow} />
         </section>
