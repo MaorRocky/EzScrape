d272fbaf4b4166c1b84d9dcc35a3c7431921c72b
Maor Rocky <maorrocky@gmail.com>
2024-11-09 19:52:01 +0200

    dev mode helpers

--- app/workflow/_components/nodes/NodeComponent.tsx
+++ app/workflow/_components/nodes/NodeComponent.tsx
@@ -6,12 +6,15 @@ import { AppNodeData } from "@/types/appNode";
 import { TaskRegistry } from "@/lib/workflow/task/Registry";
 import { NodeInput, NodeInputs } from "@/app/workflow/_components/nodes/NodeInputs";
 import { NodeOutput, NodeOutputs } from "@/app/workflow/_components/nodes/NodeOutputs";
+import { Badge } from "@/components/ui/badge";
 
+const DevMode = process.env.NEXT_PUBLIC_DEV_MODE === "true";
 const NodeComponent = memo((props: NodeProps) => {
   const nodeData = props.data as AppNodeData;
   const task = TaskRegistry[nodeData.type];
   return (
     <NodeCard nodeId={props.id} isSelected={!!props.selected}>
+      {DevMode && <Badge>Dev: {props.id}</Badge>}
       <NodeHeader taskType={nodeData.type} nodeId={props.id} />
       <NodeInputs>
         {task.inputs.map((input) => (
