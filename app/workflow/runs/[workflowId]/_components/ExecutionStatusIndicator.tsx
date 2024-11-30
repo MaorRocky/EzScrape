c9a28a3ee9cb4deeac606f2b76d2504993e64986
Maor Rocky <maorrocky@gmail.com>
2024-11-30 13:19:02 +0200

    table done

---
+++ app/workflow/runs/[workflowId]/_components/ExecutionStatusIndicator.tsx
@@ -0,0 +1,16 @@
+﻿import React from "react";
+import { WorkflowExecutionStatus } from "@/types/workflow";
+import { cn } from "@/lib/utils";
+
+const indicatorColors: Record<WorkflowExecutionStatus, string> = {
+  [WorkflowExecutionStatus.FAILED]: "bg-red-500",
+  [WorkflowExecutionStatus.PENDING]: "bg-yellow-500",
+  [WorkflowExecutionStatus.RUNNING]: "bg-blue-500",
+  [WorkflowExecutionStatus.COMPLETED]: "bg-green-500",
+};
+
+function ExecutionStatusIndicator({ status }: { status: WorkflowExecutionStatus }) {
+  return <div className={cn("w-2 h-2 rounded-full ", indicatorColors[status])} />;
+}
+
+export default ExecutionStatusIndicator;
--- app/workflow/runs/[workflowId]/_components/ExecutionTable.tsx
+++ app/workflow/runs/[workflowId]/_components/ExecutionTable.tsx
@@ -12,6 +12,12 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { DatesToDurationString } from "@/lib/helper/dates";
+import { Badge } from "@/components/ui/badge";
+import ExecutionStatusIndicator from "@/app/workflow/runs/[workflowId]/_components/ExecutionStatusIndicator";
+import { WorkflowExecutionStatus } from "@/types/workflow";
+import { CoinsIcon } from "lucide-react";
+import { formatDistanceToNow } from "date-fns";
+import { useRouter } from "next/navigation";
 
 type initialDataType = Awaited<ReturnType<typeof GetWorkflowExecutions>>;
 
@@ -22,6 +28,7 @@ function ExecutionTable({
   workflowId: string;
   initialData: initialDataType;
 }) {
+  const router = useRouter();
   const query = useQuery({
     queryKey: ["workflow-executions", workflowId],
     queryFn: () => GetWorkflowExecutions(workflowId),
@@ -45,7 +52,50 @@ function ExecutionTable({
         <TableBody className="gap-2 h-full overflow-auto">
           {query.data?.map((execution) => {
             const duration = DatesToDurationString(execution.completedAt, execution.startedAt);
-            return <TableRow key={execution.id}>// todo from here</TableRow>;
+            const formattedStartedAt =
+              execution.startedAt && formatDistanceToNow(execution.startedAt, { addSuffix: true });
+
+            return (
+              <TableRow
+                className="cursor-pointer"
+                key={execution.id}
+                onClick={() => {
+                  router.push(`/workflow/runs/${workflowId}/${execution.id}`);
+                }}>
+                <TableCell>
+                  <div className="flex flex-col">
+                    <span className="font-semibold">{execution.id}</span>
+                    <div className="text-muted-foreground text-xs">
+                      <span>Triggered Via</span>
+                      <Badge variant="outline">{execution.trigger}</Badge>
+                    </div>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <div className="flex flex-col">
+                    <div className="flex gap-2 items-center">
+                      <ExecutionStatusIndicator
+                        status={execution.status as WorkflowExecutionStatus}
+                      />
+                      <span className="font-semibold capitalize">{execution.status}</span>
+                    </div>
+                    <div className="text-muted-foreground text-xs mx-5">{duration}</div>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <div className="flex flex-col">
+                    <div className="flex gap-2 items-center">
+                      <CoinsIcon size="16" className="text-primary" />
+                      <span className="font-semibold capitalize">{execution.creditsConsumed}</span>
+                    </div>
+                    <div className="text-muted-foreground text-xs mx-5">Credits</div>
+                  </div>
+                </TableCell>
+                <TableCell className="text-right text-muted-foreground">
+                  {formattedStartedAt}
+                </TableCell>
+              </TableRow>
+            );
           })}
         </TableBody>
       </Table>
