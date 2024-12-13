2a16341fec5cab4631696cd827d39ff8457ed2c8
Maor Rocky <maorrocky@gmail.com>
2024-12-13 14:17:46 +0200

    execute cron

---
+++ app/api/workflows/cron/route.ts
@@ -0,0 +1,37 @@
+﻿import prisma from "@/lib/prisma";
+import { WorkflowStatus } from "@/types/workflow";
+import { getAppUrl } from "@/lib/helper/appUrl";
+
+export async function GET(req: Request) {
+  const now = new Date();
+  const workflows = await prisma.workflow.findMany({
+    select: { id: true },
+    where: {
+      status: WorkflowStatus.PUBLISHED,
+      cron: { not: null },
+      nextRunAt: {
+        lte: now,
+      },
+    },
+  });
+
+  console.log(workflows.length);
+
+  for (const workflow of workflows) {
+    triggerWorkflow(workflow.id);
+  }
+
+  return new Response(null, { status: 200 });
+}
+
+function triggerWorkflow(workflowId: string) {
+  const triggerApiUrl = getAppUrl(`api/workflows/execute?workflowId=${workflowId}`);
+  fetch(triggerApiUrl, {
+    headers: {
+      Authorization: `Bearer ${process.env.API_SECRET!}`,
+    },
+    cache: "no-store",
+  }).catch((er) =>
+    console.error(er.message, "Failed to trigger workflow, workflowId - ", workflowId)
+  );
+}
---
+++ app/api/workflows/execute/route.ts
@@ -0,0 +1,83 @@
+﻿import { timingSafeEqual } from "node:crypto";
+import prisma from "@/lib/prisma";
+import {
+  ExecutionPhaseStatus,
+  WorkFlowExecutionPlan,
+  WorkflowExecutionStatus,
+  WorkflowExecutionTrigger,
+} from "@/types/workflow";
+import { TaskRegistry } from "@/lib/workflow/task/Registry";
+import { ExecuteWorkflow } from "@/lib/workflow/executeWorkflow";
+
+function isValidSecret(secret: string) {
+  const API_SECRET = process.env.API_SECRET;
+  if (!API_SECRET) {
+    return false;
+  }
+  try {
+    return timingSafeEqual(Buffer.from(secret), Buffer.from(API_SECRET));
+  } catch (e) {
+    return false;
+  }
+}
+
+export async function GET(request: Request) {
+  const headers = request.headers.get("authorization");
+  if (!headers || !headers.startsWith("Bearer ")) {
+    return Response.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const secret = headers.split(" ")[1];
+  console.log("secret", secret);
+  if (!isValidSecret(secret)) {
+    return Response.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const { searchParams } = new URL(request.url);
+  const workflowId = searchParams.get("workflowId") as string;
+
+  if (!workflowId) {
+    return Response.json({ error: "bad request WorkflowId is required" }, { status: 400 });
+  }
+
+  const workflow = await prisma.workflow.findUnique({
+    where: {
+      id: workflowId,
+    },
+  });
+
+  if (!workflow) {
+    return Response.json({ error: "Workflow not found" }, { status: 404 });
+  }
+
+  const executionPlan = JSON.parse(workflow.executionPlan!) as WorkFlowExecutionPlan;
+
+  if (!executionPlan) {
+    return Response.json({ error: "Workflow execution plan not found" }, { status: 404 });
+  }
+  const userId = workflow.userId;
+  const execution = await prisma.workflowExecution.create({
+    data: {
+      workflowId: workflowId,
+      userId: userId,
+      definition: workflow.definition,
+      status: WorkflowExecutionStatus.PENDING,
+      startedAt: new Date(),
+      trigger: WorkflowExecutionTrigger.CRON,
+      phases: {
+        create: executionPlan.flatMap((phase) =>
+          phase.nodes.flatMap((node) => ({
+            userId,
+            status: ExecutionPhaseStatus.CREATED,
+            number: phase.phase,
+            node: JSON.stringify(node),
+            name: TaskRegistry[node.data.type].label,
+          }))
+        ),
+      },
+    },
+  });
+
+  await ExecuteWorkflow(execution.id);
+  return new Response(null, { status: 200 });
+}
